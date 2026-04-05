7bef614e5b417df536e4bc3ad2dfe114049954a4
Saumy05 <saumy2475@gmail.com>
2026-04-05 17:29:27 +0530

    updated the message to the log

--- server.js
+++ server.js
@@ -14,6 +14,10 @@ app.use(cors(
 ));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    res.send("Backend is running 🚀");
+});
+
 app.post("/api/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
